4ee0f71e1b0b9e1b9127d17389e2d8df5ab6be43
Gonzalo Rojas Alvarez <sisterno1998@gmail.com>
2021-06-21 17:19:23 -0500

    Prueba

--- src/components/SectionTest.js
+++ src/components/SectionTest.js
@@ -6,26 +6,38 @@ import CtaButtons from './CtaButtons';
 
 export default class SectionTest extends React.Component {
     render() {
+        // const section = _.get(this.props, 'section');
+        // const sectionId = _.get(section, 'section_id');
+        // const title = _.get(section, 'title');
+        // const image = _.get(section, 'image');
+        // const imageAlt = _.get(section, 'image_alt', '');
+        // const content = _.get(section, 'content');
+        // const actions = _.get(section, 'actions');
         const section = _.get(this.props, 'section');
         const sectionId = _.get(section, 'section_id');
         const title = _.get(section, 'title');
-        const image = _.get(section, 'image');
-        const imageAlt = _.get(section, 'image_alt', '');
+        // const image = _.get(section, 'image');
+        // const imageAlt = _.get(section, 'image_alt', '');
         const content = _.get(section, 'content');
-        const actions = _.get(section, 'actions');
+        // const actions = _.get(section, 'actions');
 
         return (
             <section id={sectionId} className="block block-text">
-                {title && <h2 className="block-title underline inner-sm">{title}</h2>}
-                {image && (
+                {title && <h2 className="block-title underline inner-sm">Prueba de alt data XD - {title}</h2>}
+                {/* {image && (
                     <div className="block-image">
                         <img src={withPrefix(image)} alt={imageAlt} />
                     </div>
-                )}
-                {content && <div className="block-content inner-sm">{markdownify(content)}</div>}
-                {!_.isEmpty(actions) && (
+                )} */}
+                {/* {content && <div className="block-content inner-sm">{markdownify(content)}</div>} */}
+                {/* {!_.isEmpty(actions) && (
                     <div className="block-buttons inner-sm">
                         <CtaButtons actions={actions} />
+                    </div>
+                )} */}
+                {!_.isEmpty(content) && (
+                    <div className="block-buttons inner-sm">
+                        XD
                     </div>
                 )}
             </section>
