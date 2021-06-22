b0f5947cf81a25a33e358ad101c7b7e4d6eb7f42
Gonzalo Rojas Alvarez <sisterno1998@gmail.com>
2021-06-22 16:33:59 -0500

    Project container added

--- src/components/SectionTest.js
+++ src/components/SectionTest.js
@@ -3,6 +3,7 @@ import _ from 'lodash';
 
 import { withPrefix, markdownify } from '../utils';
 import CtaButtons from './CtaButtons';
+import MyProjectCard from "./miniComponets/MyProjectCard"
 
 export default class SectionTest extends React.Component {
     render() {
@@ -22,8 +23,8 @@ export default class SectionTest extends React.Component {
         // const actions = _.get(section, 'actions');
 
         return (
-            <section id={sectionId} className="block block-text">
-                {title && <h2 className="block-title underline inner-sm">{title}</h2>}
+            <section id={sectionId} className="block block-text ">
+                {title && <h2 className="block-title inner-sm project-title">{title}</h2>}
                 {/* {image && (
                     <div className="block-image">
                         <img src={withPrefix(image)} alt={imageAlt} />
@@ -35,15 +36,24 @@ export default class SectionTest extends React.Component {
                         <CtaButtons actions={actions} />
                     </div>
                 )} */}
+                
                 {!_.isEmpty(content) && (
-                    content.map((e)=>{
-                        return(
-                        <div className="block-buttons inner-sm">
-                            <img src={withPrefix(e['image'])} alt={e['imageAlt']} />
-                            {e['Mini-titulo']}
-                        </div>
-                        )
-                    })
+                    <div className='project-cards-container'>
+                        {
+                            content.map((e)=>{
+                                return(
+                                    <MyProjectCard 
+                                        text={e['Mini-titulo']}
+                                        img={withPrefix(e['image'])}
+                                    />
+                                // <div className="block-buttons inner-sm">
+                                //     <img src={withPrefix(e['image'])} alt={e['imageAlt']} />
+                                //     {e['Mini-titulo']}
+                                // </div>
+                                )
+                            })
+                        }
+                    </div>
                 )}
             </section>
         );
---
+++ src/components/miniComponets/MyProjectCard.js
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from 'react';
+import style from "./style/MyProjectCard.module.css"
+export default function myProjectCard(
+    {
+        text = 'Lukuma.pe web',
+        tag = "JS | Nextjs",
+        img = "http://1.bp.blogspot.com/-EhPr4LXcywE/Udr594sPHTI/AAAAAAAAAJ4/Tv4y4CBLTPM/s400/Cristina-Otero-2.jpg"
+    },
+){
+    return (
+        <div className={style.card}>
+            <div className={style.image}>
+                <img src={img}/>
+            </div>
+            <div className={style.details}>
+                <div className={style.center}>
+                    <h1>{text}</h1>
+                    <p>{tag}</p>
+                    <ul>
+                        <li><a href="#"><i class="fa fa-github" aria-hidden="true"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    );
+}
