abcbbf4adfcdd379425f95b061e5d37832cfa186
Stackbit Code Editor <projects@stackbit.com>
2021-07-02 18:38:40 +0000

    SectionTest.js: changed

--- src/components/SectionTest.js
+++ src/components/SectionTest.js
@@ -46,6 +46,7 @@ export default class SectionTest extends React.Component {
                                         text={e['Mini-titulo']}
                                         img={withPrefix(e['image'])}
                                         tag={e['Subtext']}
+                                        github={e['Subtext']}
                                     />
                                 // <div className="block-buttons inner-sm">
                                 //     <img src={withPrefix(e['image'])} alt={e['imageAlt']} />
